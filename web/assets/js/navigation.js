da7c8ab6f24cc86af99c6ca47254b3a9498032f6
Viktor Hollanders <viktorhollanders@gmail.com>
2020-04-13 11:48:59 +0000

    rewrite heaser navigation

--- web/assets/js/navigation.js
+++
@@ -1,7 +0,0 @@
-const toggleTag = document.querySelector(".toggle-menu");
-const overlayTag = document.querySelector("nav.overlay");
-
-toggleTag.addEventListener("click", function() {
-	overlayTag.classList.toggle("open");
-	toggleTag.classList.toggle("is-active");
-});
